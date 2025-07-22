8f8e806b6866d769b506fe400cef9f751c84443c
Caio Riquelmy <crosnegocios@hotmail.com>
2025-07-22 10:54:55 -0400

    Implementation(#34): adição do endpoint de refresh JWT e configuração do CORS no backend

--- backend/metascan/settings.py
+++ backend/metascan/settings.py
@@ -31,11 +31,13 @@ INSTALLED_APPS = [
     'drf_spectacular',
     'storages',
     'encrypted_model_fields',
-    'core'
+    'core',
+    'corsheaders',
 ]
 
 # === Middleware ===
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -43,6 +45,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+# === CORS (Cross-Origin Resource Sharing) ===
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4200",  # servidor Angular
+    "http://127.0.0.1:4200",  # servidor Angular
 ]
 
 # === URLs e WSGI ===
@@ -82,7 +90,7 @@ USE_TZ = True
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
