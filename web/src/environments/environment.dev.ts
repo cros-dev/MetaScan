7148da0e8b192178c1b969a107df72bd7e963296
Caio Riquelmy <crosnegocios@hotmail.com>
2025-09-08 19:50:57 -0400

    Infra(#66): configurar environments para diferentes ambientes

--- web/src/environments/environment.dev.ts
+++ web/src/environments/environment.dev.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  
-}; 
+  production: false,
+  apiUrl: 'http://192.168.1.2:8000/',
+  appName: 'MetaScan',
+  version: '1.0.0',
+  debug: true
+};
--- web/src/environments/environment.prod.ts
+++ web/src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  
+  production: true,
+  apiUrl: 'https://api.metascan.com/',
+  appName: 'MetaScan',
+  version: '1.0.0',
+  debug: false
 }; 
