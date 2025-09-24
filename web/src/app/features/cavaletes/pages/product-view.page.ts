88bc8edc9955a3c6e094cd2bdda59ebc28d40734
Caio Riquelmy <crosnegocios@hotmail.com>
2025-09-24 14:15:42 -0400

    Refactor(#124): mover breadcrumb do componente product-view para a página seguindo o padrão de pages/

--- web/src/app/features/cavaletes/pages/product-view.page.ts
+++ web/src/app/features/cavaletes/pages/product-view.page.ts
@@ -1,15 +1,37 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { RouterModule, ActivatedRoute } from '@angular/router';
 import { ProductView } from '../components/product-view/product-view';
+import { Breadcrumb } from '../../../shared/components/breadcrumb/breadcrumb';
 
 @Component({
   selector: 'app-product-view-page',
   standalone: true,
-  imports: [RouterModule, ProductView],
+  imports: [RouterModule, ProductView, Breadcrumb],
   template: `
     <div class="layout-main">
+      <app-breadcrumb [items]="breadcrumbItems"></app-breadcrumb>
       <app-product-view></app-product-view>
     </div>
   `
 })
-export class ProductViewPage {}
+export class ProductViewPage implements OnInit {
+  breadcrumbItems: any[] = [];
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.updateBreadcrumb();
+  }
+
+  private updateBreadcrumb() {
+    const cavaleteId = this.route.snapshot.params['cavaleteId'];
+    const productCode = this.route.snapshot.params['code'];
+
+    this.breadcrumbItems = [
+      { label: 'Dashboard', url: '/' },
+      { label: 'Cavaletes', url: '/cavaletes' },
+      { label: `Cavalete ${cavaleteId}`, url: `/cavaletes/${cavaleteId}` },
+      { label: `Produto ${productCode}` }
+    ];
+  }
+}
